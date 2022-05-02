7cf6926e14b885f4dd6bf4d88713de64773c8063
Priscila Nascimento <priscilanascp93@gmail.com>
2022-05-02 15:38:19 -0300

    whatever

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App () {
+  return (
+
+
+    
+    <div className="App">
+   
+        <div className="Sucess">
+          
+          <h4>Sucess!</h4>
+          <p> Usuário cadastrado com sucesso!</p> 
+          <button> close </button>
+          </div>
+    
+          <div className="Error">
+          
+          <h4>Error</h4>
+          <p> ocorreu um erro! </p> 
+          <button> close </button>
+      
+          </div>
+
+          <div className="Alerta">
+          
+          <h4>Alerta</h4>
+          <p> Solicitação Inválida! </p> 
+          <button> close </button>
+      
+          </div>
+
+          <div className="Bem-Vindo">
+          
+          <h4>Bem-Vindo!</h4>
+       
+          
+      
+          </div>
+    </div>
+  
+  );
+}
+
+export default App;
